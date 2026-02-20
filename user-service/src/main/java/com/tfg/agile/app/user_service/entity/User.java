0312b915d1e227952f6b065bea2346467567a239
ManelMatoFdz <manel.mfernandez@udc.es>
2026-02-20 20:47:27 +0100

    Project set up

---
+++ user-service/src/main/java/com/tfg/agile/app/user_service/entity/User.java
@@ -0,0 +1,26 @@
+package com.tfg.agile.app.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
